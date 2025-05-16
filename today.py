ad76fa65f30c71cd255c6c02901658f6ef780647
debrup27 <debrupsengupta289@gmail.com>
2025-05-16 15:51:20 +0530

    Corrected Age

--- today.py
+++ today.py
@@ -322,6 +322,8 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
+    # Add this line to your svg_overwrite function
+    justify_format(root, 'age_data', age_data, 22)  # Add appropriate length parameter
     justify_format(root, 'commit_data', commit_data, 22)
     justify_format(root, 'star_data', star_data, 14)
     justify_format(root, 'repo_data', repo_data, 6)
@@ -447,7 +449,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2004, 17, 5))  # Replace with your actual birthdate
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2004, 5, 17))  # Replace with your actual birthdate
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
