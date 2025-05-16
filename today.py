d8a5030c478e73c0e52e25d93c9d4018431c8b0f
debrup27 <debrupsengupta289@gmail.com>
2025-05-16 15:49:46 +0530

    Corrected Age

--- today.py
+++ today.py
@@ -447,8 +447,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    BIRTH_DATE = datetime.datetime(2004, 5, 17)  # Your birthdate
-    age_data, age_time = perf_counter(daily_readme, BIRTH_DATE)
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2004, 17, 5))  # Replace with your actual birthdate
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
